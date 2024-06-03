1eebd00d483f4e1834f5b1f21e85f0bbb8d5ea75
AnthonyLourenco <anthony.batista@sptech.school>
2024-06-03 09:30:29 -0300

    Tabelas

--- DataBase/Tabelas.sql
+++ DataBase/Tabelas.sql
@@ -4,7 +4,7 @@ use projetoIndividual;
 create table usuario (
 idUsuario int primary key auto_increment,
 nome varchar(45) not null,
-email varchar(45) unique not null,
+email varchar(150) unique not null,
 senha varchar(45) not null
 );
 
@@ -13,6 +13,8 @@ idQuiz int primary key auto_increment,
 pontuacao varchar(45) not null,
 dtHora datetime not null
 );
+
+select * from usuario;
 
 
 
