657b18f3bad2234909b6552637418fc503ea3816
AnthonyLourenco <anthony.batista@sptech.school>
2024-06-03 09:46:15 -0300

    Tabelas e modelagem

--- DataBase/Tabelas.sql
+++ DataBase/Tabelas.sql
@@ -10,11 +10,30 @@ senha varchar(45) not null
 
 create table quiz (
 idQuiz int primary key auto_increment,
+nome varchar(45) not null,
+qtdPerguntas varchar(45) not null
+);
+
+create table historico (
+idHistorico int,
+fkUsuario int,
+fkQuiz int,
 pontuacao varchar(45) not null,
-dtHora datetime not null
+dtHora datetime not null,
+constraint pkComposta primary key (idHistorico, fkUsuario, fkQuiz),
+constraint fkUsuarioHistorico foreign key (fkUsuario) references usuario (idUsuario),
+constraint fkQuizHistorico foreign key (fkQuiz) references quiz (idQuiz)
 );
 
 select * from usuario;
+select * from quiz;
+
+
+       
+       
+       
+
+
 
 
 
